7c5b8e3ebe85ea9ba2fef67aaae57e73c746ca2d
GandresCoello18 <goyeselecoca@gmail.com>
2020-06-13 09:31:03 -0500

    update method connection mysql

--- db.ts
+++ db.ts
@@ -7,24 +7,31 @@ class Mysql {
   }
 
   conectar() {
-    var connection = mysql.createConnection({
+    const configuracion: any = mysql.createConnection({
       host: config.dbHost,
-      user: config.dbUser,
       port: 3306,
+      user: config.dbUser,
       password: config.dbPassword,
       database: config.dbName,
     });
 
-    connection.connect((err: any) => {
-      if (err) {
-        console.error(new Error(err));
-      } else {
-        console.log("conectado con exito");
-      }
-    });
+    const connection = mysql.createConnection(configuracion);
 
-    connection.on("err", (err: any) => {
-      if (err) console.log(err);
+    connection.connect(function (err) {
+      if (err) {
+        console.log("error when connecting to db:", err);
+        //setTimeout(handleDisconnect, 2000);
+        connection.on("error", function (err) {
+          console.log("db error", err);
+          if (err.code === "PROTOCOL_CONNECTION_LOST") {
+            dataBase.conectar();
+          } else {
+            throw err;
+          }
+        });
+      }
+      //handleDisconnect();
+      console.log("db conectada");
     });
 
     return connection;
